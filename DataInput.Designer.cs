d61ea40ad7540ca1919c1f34bbf5c31b1b31719b
ACT-STUDENT <ACT-STUDENT@LAB3-PC18>
2025-05-13 14:50:35 +0800

    Need Logs

--- DataInput.Designer.cs
+++ DataInput.Designer.cs
@@ -384,6 +384,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.ClientSize = new System.Drawing.Size(385, 630);
             this.ControlBox = false;
             this.Controls.Add(this.lblMessage);
